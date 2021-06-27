55134ff6e055a6c91536cb0344e371532f10ed26
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-06-27 11:21:57 +0100

    add grayscale conversion

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
+use std::path::Path;
 use std::env;
+use std::fs;
 
 use image::imageops;
 use image::io::Reader;
 
 // use image::GenericImageView;
 fn main() {
-    // let image = Reader::open("assets/wallpaperflare.com_wallpaper.jpg").unwrap().decode().unwrap();
-    // let grayscale = imageops::grayscale(&image);
-    // grayscale.save("assets/grayscale.jpg").unwrap();
 
     let args: Vec<String> = env::args().collect();
+
     // println!("args {:?}", args[1]);
     // cargo run convert input to output grayscale
-    let input_image: &String = &args[2];
-    let output_image: &String= &args[4];
-    let operation: &String = &args[5];
+    let input: &String = &args[2];
+    // let output: &String= &args[4];
+    // let operation: &String = &args[5];
 
-    convert_to_grayscale(input_image, output_image);
+    convert_to_grayscale(input);
 }
 
-fn convert_to_grayscale(input: &String, output: &String){
-    let input = Reader::open(input).unwrap().decode().unwrap();
-    imageops::grayscale(&input).save(&output).unwrap();
+fn convert_to_grayscale(input: &String){
+    let decoded_input = Reader::open(input).unwrap().decode().unwrap();
+    let output_path = "C:/Users/User/Documents/Image Shifter";
+    fs::create_dir_all(&output_path).unwrap();
+    
+    let filename = Path::new(&input).file_name().unwrap().to_str().unwrap();
+    let mut output = String::from(output_path);
+    output.push_str("/");
+    output.push_str(filename);
+
+    imageops::grayscale(&decoded_input).save(output).unwrap();
 } 
 
-// fn opening_error(message: &str){
-//     println!("{}", message)
-// }
+
+
+
