cf12a4df4918a677693cb2a3b1dc1b91d50147bf
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-06-27 19:39:15 +0100

    add Config struct and action condition

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,17 @@ fn main() {
     // println!("args {:?}", args[1]);
     // cargo run convert input to output grayscale
     let input: &String = &args[2];
-    // let output: &String= &args[4];
-    // let operation: &String = &args[5];
+    let action: &String = &args[4];
+    let config = Config::new(input.to_owned(), action.to_owned());
 
-    convert_to_grayscale(input);
+    let action = config.action.to_uppercase();
+
+   if action == "GRAYSCALE".to_owned() {
+    convert_to_grayscale(&config.input);
+   }
+    
 }
-
+/// Convert input to grayscale
 fn convert_to_grayscale(input: &String){
     let decoded_input = Reader::open(input).unwrap().decode().unwrap();
     let output_path = "C:/Users/User/Documents/Image Shifter";
@@ -32,6 +37,24 @@ fn convert_to_grayscale(input: &String){
     imageops::grayscale(&decoded_input).save(output).unwrap();
 } 
 
+struct Config {
+    input: String,
+    action: String
+}
 
+impl Config {
+    fn new(input: String, action: String) -> Self { Self { input, action } }
+}
+
+// enum Actions {
+//     Grayscale
+// }
+
+// impl Actions {
+//     /// Returns `true` if the actions is [`Grayscale`].
+//     fn is_grayscale(&self) -> bool {
+//         matches!(self, Self::Grayscale)
+//     }
+// }
 
 
