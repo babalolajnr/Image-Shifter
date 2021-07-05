fe9de73010d23b3c0a432f0f96d8c4bb84f56d40
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-07-05 06:59:13 +0100

    add explicit types

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,23 @@ use config::Config;
 use converter::Converter;
 use std::env;
 
-// use image::GenericImageView;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // cargo run convert input output grayscale
     let input: &String = &args[2];
     let action: &String = &args[3];
-    let config = Config::new(input.to_owned(), action.to_owned());
+    let config: Config = Config::new(input.to_owned(), action.to_owned());
 
-    let action = config.action.to_uppercase();
+    let action: String = config.action.to_uppercase();
 
-    let converter = Converter::new(config.input);
+    let converter: Converter = Converter::new(config.input);
 
-    match action.as_str() {
+    let output_path: Result<String, image::ImageError> = match action.as_str() {
         "GRAYSCALE" => converter.convert_to_grayscale(),
         "BRIGHTEN" => converter.brighten_image(),
         _ => panic!("Action is not available"),
-    }
+    };
+
+    println!("Converted successfully \nLocation: {}", output_path.unwrap());
 }
