5e5f32930360bbbf96520d1aca436219f67f411e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-07-10 12:11:04 +0100

    reafactor to use structopt to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-mod config;
 mod converter;
 mod constants;
+mod opt;
 
-use config::Config;
 use converter::Converter;
-use std::env;
-
+use opt::Opt;
+use structopt::StructOpt;
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    // cargo run convert input output grayscale
-    let input: &String = &args[2];
-    let action: &String = &args[3];
-    let config: Config = Config::new(input.to_owned(), action.to_owned());
+    let opt = Opt::from_args();
+    let converter: Converter = Converter::new(opt.input);
 
-    let action: String = config.action.to_uppercase();
-
-    let converter: Converter = Converter::new(config.input);
-
-    let output_path: Result<String, image::ImageError> = match action.as_str() {
-        "GRAYSCALE" => converter.convert_to_grayscale(),
-        "BRIGHTEN" => converter.brighten_image(),
-        _ => panic!("Action is not available"),
+   let output_path: Result<String, image::ImageError> = match opt.action {
+        _grayscale => converter.convert_to_grayscale()
     };
 
     println!("Converted successfully \nLocation: {}", output_path.unwrap());
+
 }
