28c93343ecbf8cdf370116ca6cb040ce71537e74
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-07-11 23:17:59 +0100

    fix pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-mod converter;
 mod constants;
+mod converter;
 mod opt;
 
 use converter::Converter;
+use opt::Action;
 use opt::Opt;
 use structopt::StructOpt;
 fn main() {
-
     let opt = Opt::from_args();
+
     let converter: Converter = Converter::new(opt.input);
 
-   let output_path: Result<String, image::ImageError> = match opt.action {
-        _grayscale => converter.convert_to_grayscale()
+    let output_path: Result<String, image::ImageError> = match opt.action {
+        Action::Brighten { value } => converter.brighten_image(value),
+        Action::Grayscale => converter.convert_to_grayscale(),
     };
 
-    println!("Converted successfully \nLocation: {}", output_path.unwrap());
-
+    println!(
+        "Converted successfully \nLocation: {}",
+        output_path.unwrap()
+    );
 }
